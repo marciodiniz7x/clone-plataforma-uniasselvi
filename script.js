caee6c69925f2b762fc5e4ed28b6119847614c64
Márcio Diniz <marcio.sccp10@gmail.com>
2023-03-13 15:42:24 -0300

    ats

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// Função para o botão de colapso
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
