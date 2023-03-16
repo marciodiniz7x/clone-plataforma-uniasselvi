cb869dc3a12b55007f1b0a5769d595f75ac38d0c
Márcio Diniz <marcio.sccp10@gmail.com>
2023-03-15 21:18:01 -0300

    Ats

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 
 // Função para o botão de colapso
 function toggleMenu() {
-    var menu = document.getElementById("menu");
+    const menu = document.getElementById("menu");
+    const bannerShadow = document.querySelector('.banner-inicio');
+    const rodape = document.querySelector('.footer01');
     if (menu.style.display === "block") {
         menu.style.display = "none";
+        bannerShadow.style.borderLeft = "none";
+        rodape.style.borderLeft = "none";
     } else {
         menu.style.display = "block";
+        bannerShadow.style.borderLeft = "2px solid #E6E6E6";
+        rodape.style.borderLeft = "2px solid #E6E6E6";
     }
 }
