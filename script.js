d5b71cc6f1f0b27bd9f0bd85b131cde337ffb21a
Márcio Diniz <marcio.sccp10@gmail.com>
2023-03-16 03:06:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+let menuAberto = false;
 // Função para o botão de colapso
 function toggleMenu() {
     const lateral = document.querySelector(".lateral");
@@ -6,21 +7,20 @@ function toggleMenu() {
     const rodape = document.querySelector('.footer01');
 
     const topo = document.querySelector('.topo');
-    let menuAberto = true;
+
 
     if (menuAberto === false) {
-
-        alert('menu aberto')
-
-        lateral.style.transform = "translateX(0%)";
-        topo.style.backgroundColor = 'var(--bg-preto)';
-        menuAberto = true;
         
-    } else {
-
-        alert('menu fechado')
         lateral.style.transform = "translateX(-100%)";
         topo.style.background = 'var(--bg-preto-transparente)';
+        menuAberto = true;
+
+    } else {
+        
+        lateral.style.transform = "translateX(0%)";
+        topo.style.backgroundColor = 'var(--bg-preto)';
         menuAberto = false;
+        
     }
+
 }
