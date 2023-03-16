a432c21c887fc2e331e609bbafd9a323f3219255
Márcio Diniz <marcio.sccp10@gmail.com>
2023-03-15 22:24:32 -0300

    Ats

--- script.js
+++ script.js
@@ -4,14 +4,19 @@ function toggleMenu() {
     const menu = document.getElementById("menu");
     const bannerShadow = document.querySelector('.banner-inicio');
     const rodape = document.querySelector('.footer01');
+
+    const topo = document.querySelector('.topo');
+
     if (menu.style.display === "none") {
 
         menu.style.display = "block";
         bannerShadow.style.borderLeft = "2px solid #E6E6E6";
         rodape.style.borderLeft = "2px solid #E6E6E6";
+
+        topo.style.position = 'var(--position-static)';
         
     } else {
-        
+
         menu.style.display = "none";
         bannerShadow.style.borderLeft = "none";
         rodape.style.borderLeft = "none";
