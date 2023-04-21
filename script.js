7153901a3792d16c16cf86f4f95d755416ff3d94
Márcio Diniz <marcio.sccp10@gmail.com>
2023-04-21 16:03:16 -0300

    Updates

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-
+// Seleciona elementos
 let menuAberto = false;
 const subitem01 = document.querySelector('.subitem01');
 const subitem02 = document.querySelector('.subitem02');
 const subitem03 = document.querySelector('.subitem03');
 const containerUni01 = document.querySelector('.container-uni01');
 const setaUni01 = document.querySelector('.seta-uni01');
+
 // Função para o botão de colapso
 function toggleMenu() {
     const lateral = document.querySelector(".lateral");
@@ -14,6 +15,7 @@ function toggleMenu() {
 
     if (menuAberto === false) {
         
+        // Caso fechado abrir menu
         lateral.style.transform = "translateX(-100%)";
         banner.style.left = '-280px';
         lateral.style.boxShadow = 'none';
@@ -22,6 +24,7 @@ function toggleMenu() {
 
     } else {
         
+        // Caso aberto fechar menu
         lateral.style.transform = "translateX(0%)";
         banner.style.left = '0';
         topo.style.backgroundColor = 'var(--bg-preto)';
